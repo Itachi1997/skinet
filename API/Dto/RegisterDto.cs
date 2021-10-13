228fdcfc2267a1e9990109dda1446dff001e1386
Waseem Fiaz <waseem@etechtics.com>
2021-10-13 10:28:15 +0500

    End of section 16

--- API/Dto/RegisterDto.cs
+++ API/Dto/RegisterDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,13 @@ namespace API.Dto
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string email { get; set; }
+        [Required]
+        // [RegularExpression()]
         public string Password { get; set; }
     }
 }
--- API/Extentions/SwaggerServiceExtentions.cs
+++ API/Extentions/SwaggerServiceExtentions.cs
@@ -11,6 +11,23 @@ namespace API.Extentions
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirments = new OpenApiSecurityRequirement { { securitySchema, new[] { "Bearer" } } };
+                c.AddSecurityRequirement(securityRequirments);
             });
 
             return services;
